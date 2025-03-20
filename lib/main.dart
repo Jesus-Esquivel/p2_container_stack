95bd041117710bac1bc9781c33bb7d9712ad6021
JESUS_ADRIAN_ESQUIVEL_MARTINEZ <a.22308051281176@cbtis128.edu.mx>
2025-03-20 19:03:31 +0000

    a

--- lib/main.dart
+++ lib/main.dart
@@ -8,35 +8,71 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Jesus Esquivel/1176'),
+          titleTextStyle: const TextStyle(color: Colors.white, fontSize: 25),
+          backgroundColor: Colors.red,
+        ),
+        body: Center(
+          child: const RoomProperties(),
+        ),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class RoomProperties extends StatelessWidget {
+  const RoomProperties({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.blue[50],
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      child: const Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Propiedades de la Habitación:',
+            style: TextStyle(
+                fontSize: 22, fontWeight: FontWeight.bold), // Tamaño aumentado
+          ),
+          SizedBox(height: 10),
+          Text(
+            '1. Tamaño: 20 m²',
+            style: TextStyle(fontSize: 20), // Tamaño aumentado
+          ),
+          Text(
+            '2. Número de camas: 2',
+            style: TextStyle(fontSize: 20), // Tamaño aumentado
+          ),
+          Text(
+            '3. Baño privado: Sí',
+            style: TextStyle(fontSize: 20), // Tamaño aumentado
+          ),
+          Text(
+            '4. Vista al mar: No',
+            style: TextStyle(fontSize: 20), // Tamaño aumentado
+          ),
+          Text(
+            '5. WiFi gratuito: Sí',
+            style: TextStyle(fontSize: 20), // Tamaño aumentado
+          ),
+        ],
       ),
     );
   }
